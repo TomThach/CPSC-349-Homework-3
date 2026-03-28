ac1e35e7ab189cc850a3aff3e36882be7b1f2b27
Tom Thach <tomthach000@gmail.com>
2026-03-27 19:27:14 -0700

    added movie card

---
+++ movie-explorer/src/components/MovieCard.js
@@ -0,0 +1,19 @@
+const IMG_PATH = 'https://image.tmdb.org/t/p/w500';
+
+function MovieCard({ movie }) {
+  const { title, poster_path, vote_average, release_date } = movie;
+  const posterUrl = poster_path
+    ? IMG_PATH + poster_path
+    : 'https://via.placeholder.com/500x750?text=No+Poster';
+
+  return (
+    <div className="movie-card">
+      <img src={posterUrl} alt={title} />
+      <h3>{title}</h3>
+      <p>Release Date: {release_date || 'N/A'}</p>
+      <p>Rating: {vote_average}</p>
+    </div>
+  );
+}
+
+export default MovieCard;
